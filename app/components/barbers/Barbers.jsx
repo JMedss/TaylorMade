173b3a8aae6882ef7449053b0af4d9d6e0f778ce
Josh Meadows <joshmeadows@Joshs-Air.home>
2024-01-02 19:48:17 -0500

    correct url part2

--- app/components/barbers/Barbers.jsx
+++ app/components/barbers/Barbers.jsx
@@ -10,7 +10,7 @@ const BarbersMain = (props) => {
   const location = props.location
 
   async function getBarbers() {
-    const res = await fetch('http://taylormade.com/api/getBarbers', {
+    const res = await fetch('https://taylormade.com/api/getBarbers', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
