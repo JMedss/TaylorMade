ae284dd1f1bf02f09e60271d4386dc382d174282
Josh Meadows <joshmeadows@Joshs-Air.home>
2024-01-08 15:59:58 -0500

    fixed only delete, one prior working

--- app/api/deleteBarber/route.jsx
+++ app/api/deleteBarber/route.jsx
@@ -6,35 +6,6 @@ export async function DELETE(request) {
     const body = await request.json()
     const { id } = body
 
-    const appointmentsToDelete = await prisma.appointment.findMany({
-      where: {
-        barberId: id, 
-      },
-    })
-
-    if(appointmentsToDelete && appointmentsToDelete.length > 0) {
-      await prisma.appointment.deleteMany({
-          where: {
-            barberId: id,
-          },
-        });
-  }
-  
-
-    const servicesToDelete = await prisma.service.findMany({
-        where: {
-          barberId: id, 
-        },
-      })
-    
-    if(servicesToDelete && servicesToDelete.length > 0) {
-        await prisma.service.deleteMany({
-            where: {
-              barberId: id,
-            },
-          });
-    }
-
     const barber = await prisma.barber.delete({
         where: {
             id: id
